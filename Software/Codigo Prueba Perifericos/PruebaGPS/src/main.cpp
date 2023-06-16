0ab051f5e1563f7e5f705e7251cbf8b4d728e9e3
Leonardo Gonzalez <leogb37@gmail.com>
2023-06-16 20:13:25 -0300

    Actualizo codigo PruebaGPS

--- Software/Codigo Prueba Perifericos/PruebaGPS/src/main.cpp
+++ Software/Codigo Prueba Perifericos/PruebaGPS/src/main.cpp
@@ -1,8 +1,7 @@
 #include <Arduino.h>
-#include <SoftwareSerial.h>//incluimos SoftwareSerial
-#include <TinyGPS.h>//incluimos TinyGPS
-//#include <TinyGPSPlus.h>
-int timeOffset = -3;  // Ajuste horario en horas (ejemplo: -3 para GMT-3)
+#include <SoftwareSerial.h>//incluimos libreria SoftwareSerial
+#include <TinyGPS.h>//incluimos libreria TinyGPS
+int timeOffset = -3;  // Ajuste horario en horas (ejemplo: -3 para GMT-3 Arg)
 TinyGPS gps;//Declaramos el objeto gps
 //Declaramos pines GPS
 #define pinRX 23
@@ -35,33 +34,31 @@ if(gps.encode(c))
 
 {
 float latitude, longitude;
-gps.f_get_position(&latitude, &longitude);
+gps.f_get_position(&latitude, &longitude); //obtenemos lat y long, luego mostramos por serial
 Serial.print("Latitud/Longitud: "); 
 Serial.print(latitude,5); 
 Serial.print(", "); 
 Serial.println(longitude,5);
-gps.crack_datetime(&year,&month,&day,&hour,&minute,&second,&hundredths);
 
-//Serial.print(" HORA: "); Serial.println(hour, DEC);
-  //Serial.print("HORAAAA: "); Serial.println(hour);
-  int hora = hour + timeOffset;
+gps.crack_datetime(&year,&month,&day,&hour,&minute,&second,&hundredths); //obtenemos los datos
+  int hora = hour + timeOffset; //seteamos el uso horario Arg
   int dia= day;
-  Serial.print("HORA: "); Serial.println(hora);
-  if (hora < 0){
+  if (hora < 0){ //si hora <0 significa que en Arg todavia no paso medianoche, entonces ajustamos el dia local
     dia= day -1;
-    Serial.print("DIA: "); Serial.println(dia); 
+    
   }
+  //realizamos el ajuste de la hora Arg mediante + o -
     if (hora < 0) {
           hora += 24;
         } else if (hora >= 24) {
           hora -= 24;
         }
+//mostramos los datos
 Serial.print("Fecha: "); Serial.print(dia); Serial.print("/"); 
 Serial.print(month, DEC); Serial.print("/"); Serial.println(year);        
 Serial.print("Hora: "); Serial.print(hora); Serial.print(":"); 
 Serial.print(minute, DEC); Serial.print(":"); Serial.print(second, DEC); 
 Serial.print("."); Serial.println(hundredths, DEC);
-//Serial.print("Altitud (metros): "); Serial.println(gps.f_altitude()); 
 Serial.print("Rumbo (grados): "); Serial.println(gps.f_course()); 
 Serial.print("Velocidad(kmph): "); Serial.println(gps.f_speed_kmph());
 Serial.print("Satelites: "); Serial.println(gps.satellites());
