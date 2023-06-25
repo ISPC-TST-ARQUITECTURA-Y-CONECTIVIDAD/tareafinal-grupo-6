22320cbb10f66ac2f939e35b6491aad8d1b9f31b
Mario <mario.gonzalez@hotmail.com.ar>
2023-06-25 02:58:24 -0300

    condicional para envio solo si se mueve el auto

--- Software/traker/src/main.cpp
+++ Software/traker/src/main.cpp
@@ -1,43 +1,45 @@
 #include <Arduino.h>
 #define TINY_GSM_MODEM_SIM800
 #include <SoftwareSerial.h>
-#include <TinyGPS.h>//incluimos libreria TinyGPS
-int timeOffset = -3;  // Ajuste horario en horas (ejemplo: -3 para GMT-3 Arg)
-TinyGPS gps;//Declaramos el objeto gps
+#include <TinyGPS.h> //incluimos libreria TinyGPS
+int timeOffset = -3; // Ajuste horario en horas (ejemplo: -3 para GMT-3 Arg)
+TinyGPS gps;         // Declaramos el objeto gps
 int year;
-byte month, day, hour, minute, second, hundredths; 
+byte month, day, hour, minute, second, hundredths;
 unsigned long chars;
 unsigned short sentences, failed_checksum;
 String idtracker = "1";
-float num1 = 0;
-float num2 = 0;
+float lat_b = 0;
+float lon_b = 0;
 
 SoftwareSerial SerialAT(5, 18);
 SoftwareSerial serialgps(15, 2);
 
 #include <TinyGsmClient.h>
 
-const char apn[]      = "gprs.personal.com";
+const char apn[] = "gprs.personal.com";
 const char gprsUser[] = "gprs";
 const char gprsPass[] = "gprs";
-const char* broker = "brokergrupo6.ddns.net";
-const char* topic = "/datos/traker/";
+const char *broker = "brokergrupo6.ddns.net";
+const char *topic = "/datos/traker/";
 #include <PubSubClient.h>
 
-TinyGsm       modem(SerialAT);
+TinyGsm modem(SerialAT);
 TinyGsmClient client(modem);
-PubSubClient  mqtt(client);
+PubSubClient mqtt(client);
 
 uint32_t lastReconnectAttempt = 0;
 
-void mqttCallback(char* topic, byte* payload, unsigned int len) {
-}
+void mqttCallback(char *topic, byte *payload, unsigned int len)
+{}
 
-boolean mqttConnect() {
+boolean mqttConnect()
+{
   Serial.print("Conectando con ");
   Serial.println(broker);
   boolean status = mqtt.connect("GsmClientTest");
-  if (status == false) {
+  if (status == false)
+  {
     Serial.println("fail");
     return false;
   }
@@ -46,109 +48,133 @@ boolean mqttConnect() {
   return mqtt.connected();
 }
 
-void red_gsm(){
-    Serial.println("Network disconnected");
-    if (!modem.waitForNetwork(180000L, true)) {
-      Serial.println(" Error de conexion !!!!");
-      delay(10000);
-      return;
-    }
-    if (modem.isNetworkConnected()) {
-      Serial.println("Network re-connected");
-    }
-}
-
-void gprs_gsm(){
-        Serial.println("GPRS disconnected!");
-      Serial.print(F("Connecting to "));
-      Serial.print(apn);
-      if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-        Serial.println(" fail");
-        delay(10000);
-        return;
-      }
-      if (modem.isGprsConnected()) { Serial.println("GPRS reconnected"); }
-    }
-
-void broker_gsm(){
-    uint32_t t = millis();
-    if (t - lastReconnectAttempt > 10000L) {
-      lastReconnectAttempt = t;
-      if (mqttConnect()) { lastReconnectAttempt = 0; }
-    }
-    delay(100);
+void red_gsm()
+{
+  Serial.println("Network disconnected");
+  if (!modem.waitForNetwork(180000L, true))
+  {
+    Serial.println(" Error de conexion !!!!");
+    delay(10000);
     return;
+  }
+  if (modem.isNetworkConnected())
+  {
+    Serial.println("Network re-connected");
+  }
 }
 
-void setup() {
-    Serial.begin(9600);
-    SerialAT.begin(9600);
-    serialgps.begin(9600);
-    Serial.println("Iniciando modem...");
-    modem.restart();
-    delay(6000);
-    Serial.print(F("Conectando con "));
-    Serial.println(apn);
-    if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+void gprs_gsm()
+{
+  Serial.println("GPRS disconnected!");
+  Serial.print(F("Connecting to "));
+  Serial.print(apn);
+  if (!modem.gprsConnect(apn, gprsUser, gprsPass))
+  {
+    Serial.println(" fail");
+    delay(10000);
+    return;
+  }
+  if (modem.isGprsConnected())
+  {
+    Serial.println("GPRS reconnected");
+  }
+}
+
+void broker_gsm()
+{
+  uint32_t t = millis();
+  if (t - lastReconnectAttempt > 10000L)
+  {
+    lastReconnectAttempt = t;
+    if (mqttConnect())
+    {
+      lastReconnectAttempt = 0;
+    }
+  }
+  delay(100);
+  return;
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  SerialAT.begin(9600);
+  serialgps.begin(9600);
+  Serial.println("Iniciando modem...");
+  modem.restart();
+  delay(6000);
+  Serial.print(F("Conectando con "));
+  Serial.println(apn);
+  if (!modem.gprsConnect(apn, gprsUser, gprsPass))
+  {
     Serial.println("Error en la conexion !!!!!!!");
     delay(10000);
     return;
-    }
-    Serial.println("Conexion Exitosa !!");
-    if (modem.isGprsConnected()) { Serial.println("GPRS conectado"); }
-    mqtt.setServer(broker, 1883);
-    mqtt.setCallback(mqttCallback);
+  }
+  Serial.println("Conexion Exitosa !!");
+  if (modem.isGprsConnected())
+  {
+    Serial.println("GPRS conectado");
+  }
+  mqtt.setServer(broker, 1883);
+  mqtt.setCallback(mqttCallback);
 }
-void loop() {
-  if (!modem.isNetworkConnected()) {
-      red_gsm();
-  }
-  if (!modem.isGprsConnected()) {
-      gprs_gsm();
-    }
-  if (!mqtt.connected()) {
-      broker_gsm();
-  }
-    mqtt.loop();
 
-while(serialgps.available()){
+void loop()
 {
-int c = serialgps.read(); 
-if(gps.encode(c)) {
-float latitude, longitude;
-gps.f_get_position(&latitude, &longitude); //obtenemos lat y long, luego mostramos por serial
-Serial.println(latitude,3); 
-Serial.println(longitude,3);
-
-gps.crack_datetime(&year,&month,&day,&hour,&minute,&second,&hundredths); //obtenemos los datos
-  int hora = hour + timeOffset; //seteamos el uso horario Arg
-  int dia= day;
-  if (hora < 0){ //si hora <0 significa que en Arg todavia no paso medianoche, entonces ajustamos el dia local
-    dia= day -1;
-    }
-  //realizamos el ajuste de la hora Arg mediante + o -
-    if (hora < 0) {
+  if (!modem.isNetworkConnected())
+  {
+    red_gsm();
+  }
+  if (!modem.isGprsConnected())
+  {
+    gprs_gsm();
+  }
+  if (!mqtt.connected())
+  {
+    broker_gsm();
+  }
+  mqtt.loop();
+  while (serialgps.available())
+  {
+    {
+      int c = serialgps.read();
+      if (gps.encode(c))
+      {
+        float latitude, longitude;
+        gps.f_get_position(&latitude, &longitude);                                     // obtenemos lat y long, luego mostramos por serial
+        gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths); // obtenemos los datos
+        int hora = hour + timeOffset;                                                  // seteamos el uso horario Arg
+        int dia = day;
+        if (hora < 0)
+        { // si hora <0 significa que en Arg todavia no paso medianoche, entonces ajustamos el dia local
+          dia = day - 1;
+        }
+        // realizamos el ajuste de la hora Arg mediante + o -
+        if (hora < 0)
+        {
           hora += 24;
-        } else if (hora >= 24) {
+        }
+        else if (hora >= 24)
+        {
           hora -= 24;
         }
-String dato=String(year)+String("-")+String(month)+String("-")+String(dia)+","+\
-            String(hora)+String(":")+String(minute)+String(":")+String(second)+","+\
-            idtracker+","+\
-            String(latitude,5)+","+\
-            String(longitude,5)+","+\
-            String(gps.f_speed_kmph());
-mqtt.publish(topic, dato.c_str());
-delay(4000);
+        float lat_a = round(latitude * 1000.0) / 1000.0;
+        float lon_a = round(longitude * 1000.0) / 1000.0;
+        if ((lat_a != lat_b) || (lon_a != lon_b)) {
+          String dato = String(year) + String("-") + String(month) + String("-") + String(dia) + "," +
+                        String(hora) + String(":") + String(minute) + String(":") + String(second) + "," +
+                        idtracker + "," +
+                        String(latitude, 5) + "," +
+                        String(longitude, 5) + "," +
+                        String(gps.f_speed_kmph());
+          mqtt.publish(topic, dato.c_str());
+          delay(3000);
+          lat_b = round(latitude * 1000.0) / 1000.0;
+          lon_b = round(longitude * 1000.0) / 1000.0;
+        }
+        delay(1000);
+      }
+    }
+  }
 }
-
-
-
-
-
-
-
-    }
-    }
-   }
-  
